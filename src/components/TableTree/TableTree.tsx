543f9f9d4d2651a0de76632fed693e71663af470
PrateekSahu9808 <mailprateeksahu@gmail.com>
2024-10-21 22:55:48 +0530

    expand and collapse

--- src/components/TableTree/TableTree.tsx
+++ src/components/TableTree/TableTree.tsx
@@ -1,5 +1,163 @@
+// import './TableTree.scss';
+// import React, { useEffect, useRef, useState } from 'react';
+
+// interface ColumnDataProps {
+//   name: string;
+//   accessor: string;
+//   width: string;
+//   isClickable?: boolean;
+// }
+
+// interface ObjectProps {
+//   [key: string]: any;
+// }
+
+// interface TableTreeProps {
+//   withCheckBox: boolean;
+//   columnsData: Array<ColumnDataProps>;
+//   treeData: Array<ObjectProps>;
+//   onClick?: (
+//     event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+//     data: any
+//   ) => void;
+// }
+
+// const TableTree = ({
+//   withCheckBox,
+//   columnsData,
+//   treeData,
+//   // onClick = () => {},
+// }: TableTreeProps) => {
+//   const calculateTotalChildrenHeight = (node: any): number => {
+//     if (!node.children || node.children.length === 0) {
+//       return 1; // Base height for a single node
+//     }
+
+//     let totalHeight = 0; // Start from 0 to sum heights
+//     node.children.forEach((child: any) => {
+//       totalHeight += calculateTotalChildrenHeight(child); // Recursive height calculation
+//     });
+//     return totalHeight + 1; // Add one for the current node
+//   };
+
+//   const TreeNode = ({ node, level, isLast }: any) => {
+//     const nodeRef = useRef<HTMLTableRowElement | null>(null);
+//     const [nodeHeight, setNodeHeight] = useState<number>(0);
+//     const [totalChildHeight, setTotalChildHeight] = useState<number>(0);
+
+//     useEffect(() => {
+//       if (nodeRef.current) {
+//         const height = nodeRef.current.offsetHeight;
+//         setNodeHeight(height);
+//       }
+//     }, [nodeRef]);
+
+//     const totalHeight = calculateTotalChildrenHeight(node);
+
+//     useEffect(() => {
+//       if (node.children && node.children.length > 0) {
+//         const height = node.children.reduce((acc: number, child: any) => {
+//           return acc + calculateTotalChildrenHeight(child);
+//         }, 0);
+//         setTotalChildHeight(height);
+//       }
+//     }, [node.children]);
+
+//     return (
+//       <>
+//         <tr
+//           ref={nodeRef}
+//           className={`node-li ${node.children ? 'has-children' : ''} ${
+//             isLast ? 'is-last' : ''
+//           }`}
+//           style={
+//             {
+//               '--level': level,
+//               '--total-height': totalHeight,
+//               '--total-child-height': `${totalChildHeight * 25}px`, // Adjust based on the node height
+//               '--node-height': `${nodeHeight}px`, // Set node height dynamically
+//             } as React.CSSProperties
+//           }
+//         >
+//           <td className="title-container">
+//             <span className="folder">
+//               {node.folder && (
+//                 <svg
+//                   width="16"
+//                   height="17"
+//                   viewBox="0 0 16 17"
+//                   fill="none"
+//                   xmlns="http://www.w3.org/2000/svg"
+//                 >
+//                   <path
+//                     d="M9.36047 8.49999L4.76721 13.0933C4.42253 13.438 4.42253 13.9968 4.76721 14.3415C5.11189 14.6862 5.67073 14.6862 6.01538 14.3415L11.2328 9.12408C11.2328 9.12407 11.2328 9.12407 11.2328 9.12406C11.3984 8.95857 11.4913 8.73404 11.4913 8.49999C11.4913 8.26594 11.3984 8.0414 11.2328 7.87592C11.2328 7.87591 11.2328 7.8759 11.2328 7.8759L6.01538 2.65852C5.67074 2.31382 5.11189 2.31383 4.76721 2.65852C4.42253 3.00321 4.42253 3.56196 4.76721 3.90665L4.83792 3.83594L4.76721 3.90665L9.36047 8.49999Z"
+//                     fill="#71347B"
+//                     stroke="#71347B"
+//                     strokeWidth="0.2"
+//                   />
+//                 </svg>
+//               )}
+//               {withCheckBox && (
+//                 <input
+//                   style={{ width: '14px', height: '14px', marginLeft: '8px' }}
+//                 />
+//               )}
+//             </span>
+//             <div
+//               className="title"
+//               style={{ fontWeight: node.folder ? 600 : 400 }}
+//             >
+//               {node.title}
+//             </div>
+//           </td>
+
+//           {columnsData.map((column: any) => (
+//             <td key={column.accessor} style={{ width: column.width }}>
+//               {node[column.accessor]}
+//             </td>
+//           ))}
+//         </tr>
+
+//         {node.children &&
+//           node.children.length > 0 &&
+//           renderTree(node.children, level + 1)}
+//       </>
+//     );
+//   };
+
+//   const renderTree = (nodes: any, level = 0) => {
+//     return nodes.map((node: any, index: number) => {
+//       const isLast = index === nodes.length - 1;
+//       return (
+//         <TreeNode key={node.key} node={node} level={level} isLast={isLast} />
+//       );
+//     });
+//   };
+
+//   return (
+//     <div className="tree-container">
+//       <table>
+//         <thead>
+//           <tr>
+//             <th style={{ minWidth: '760px' }}>Execution</th>
+//             {columnsData.map((column: any) => (
+//               <th key={column.accessor} style={{ width: column.width }}>
+//                 {column.name}
+//               </th>
+//             ))}
+//           </tr>
+//         </thead>
+//         <tbody className="tree">{renderTree(treeData)}</tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default TableTree;
+
 import './TableTree.scss';
-import React, { useEffect, useRef, useState } from 'react';
+import Checkbox from '../Checkbox/Checkbox.js';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 interface ColumnDataProps {
   name: string;
@@ -26,68 +184,78 @@ const TableTree = ({
   withCheckBox,
   columnsData,
   treeData,
-  // onClick = () => {},
+  onClick = () => {},
 }: TableTreeProps) => {
+  const [expandedNodes, setExpandedNodes] = useState<Set<ObjectProps>>(new Set());
+
+  // Function to calculate total children height
   const calculateTotalChildrenHeight = (node: any): number => {
     if (!node.children || node.children.length === 0) {
-      return 1; // Base height for a single node
+      return 1; // 1 for the current node itself
     }
 
-    let totalHeight = 0; // Start from 0 to sum heights
-    node.children.forEach((child: any) => {
-      totalHeight += calculateTotalChildrenHeight(child); // Recursive height calculation
-    });
-    return totalHeight + 1; // Add one for the current node
+    let totalHeight = 1; // Start with 1 for the current node
+    if (expandedNodes.has(node)) {
+      node.children.forEach((child: any) => {
+        totalHeight += calculateTotalChildrenHeight(child);
+      });
+    }
+    return totalHeight; // Return the total height including children
   };
 
   const TreeNode = ({ node, level, isLast }: any) => {
     const nodeRef = useRef<HTMLTableRowElement | null>(null);
     const [nodeHeight, setNodeHeight] = useState<number>(0);
-    const [totalChildHeight, setTotalChildHeight] = useState<number>(0);
+    const [totalChildrenHeight, setTotalChildrenHeight] = useState<number>(0);
 
-    useEffect(() => {
+    const isExpanded = level === 0 || expandedNodes.has(node);
+
+    useLayoutEffect(() => {
       if (nodeRef.current) {
-        const height = nodeRef.current.offsetHeight;
-        setNodeHeight(height);
+        setNodeHeight(nodeRef.current.offsetHeight);
       }
-    }, [nodeRef]);
+      const childrenHeight = calculateTotalChildrenHeight(node);
+      setTotalChildrenHeight(childrenHeight);
+    }, [isExpanded, node]);
 
-    const totalHeight = calculateTotalChildrenHeight(node);
-
-    useEffect(() => {
-      if (node.children && node.children.length > 0) {
-        const height = node.children.reduce((acc: number, child: any) => {
-          return acc + calculateTotalChildrenHeight(child);
-        }, 0);
-        setTotalChildHeight(height);
-      }
-    }, [node.children]);
+    const handleToggleExpand = () => {
+      setExpandedNodes((prev) => {
+        const newExpandedNodes = new Set(prev);
+        if (newExpandedNodes.has(node)) {
+          newExpandedNodes.delete(node); // Collapse the node
+        } else {
+          newExpandedNodes.add(node); // Expand the node
+        }
+        return newExpandedNodes;
+      });
+    };
 
     return (
       <>
         <tr
           ref={nodeRef}
-          className={`node-li ${node.children ? 'has-children' : ''} ${
-            isLast ? 'is-last' : ''
-          }`}
-          style={
-            {
-              '--level': level,
-              '--total-height': totalHeight,
-              '--total-child-height': `${totalChildHeight * 25}px`, // Adjust based on the node height
-              '--node-height': `${nodeHeight}px`, // Set node height dynamically
-            } as React.CSSProperties
-          }
+          className={`node-li ${node.children ? 'has-children' : ''} ${isLast ? 'is-last' : ''}`}
+          style={{
+            '--level': level,
+            '--node-height': `${nodeHeight}px`,
+            '--total-children-height': `${totalChildrenHeight * nodeHeight}px`,
+          } as React.CSSProperties}
+          onClick={(event) => onClick(event, node)}
         >
           <td className="title-container">
-            <span className="folder">
+            <span className="folder" onClick={handleToggleExpand} style={{ cursor: 'pointer' }}>
               {node.folder && (
                 <svg
                   width="16"
-                  height="17"
+                  height="16"
                   viewBox="0 0 16 17"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
+                  style={{
+                    marginLeft: '8px',
+                    transform: isExpanded ? 'rotate(90deg)' : 'rotate(0deg)',
+                    transition: 'transform 0.3s ease-in-out',
+                  }}
                 >
                   <path
                     d="M9.36047 8.49999L4.76721 13.0933C4.42253 13.438 4.42253 13.9968 4.76721 14.3415C5.11189 14.6862 5.67073 14.6862 6.01538 14.3415L11.2328 9.12408C11.2328 9.12407 11.2328 9.12407 11.2328 9.12406C11.3984 8.95857 11.4913 8.73404 11.4913 8.49999C11.4913 8.26594 11.3984 8.0414 11.2328 7.87592C11.2328 7.87591 11.2328 7.8759 11.2328 7.8759L6.01538 2.65852C5.67074 2.31382 5.11189 2.31383 4.76721 2.65852C4.42253 3.00321 4.42253 3.56196 4.76721 3.90665L4.83792 3.83594L4.76721 3.90665L9.36047 8.49999Z"
@@ -97,16 +265,9 @@ const TableTree = ({
                   />
                 </svg>
               )}
-              {withCheckBox && (
-                <input
-                  style={{ width: '14px', height: '14px', marginLeft: '8px' }}
-                />
-              )}
+              {withCheckBox && <Checkbox style={{ marginLeft: '8px' }} />}
             </span>
-            <div
-              className="title"
-              style={{ fontWeight: node.folder ? 600 : 400 }}
-            >
+            <div className="title" style={{ fontWeight: node.folder ? 600 : 400 }}>
               {node.title}
             </div>
           </td>
@@ -118,7 +279,9 @@ const TableTree = ({
           ))}
         </tr>
 
-        {node.children &&
+        {/* Render children only if the node is expanded */}
+        {isExpanded &&
+          node.children &&
           node.children.length > 0 &&
           renderTree(node.children, level + 1)}
       </>
@@ -129,7 +292,7 @@ const TableTree = ({
     return nodes.map((node: any, index: number) => {
       const isLast = index === nodes.length - 1;
       return (
-        <TreeNode key={node.key} node={node} level={level} isLast={isLast} />
+        <TreeNode key={index} node={node} level={level} isLast={isLast} />
       );
     });
   };
